b38980b1ab958745867d3171c208a82f83705e21
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-01-02 23:36:09 +0100

    workaround Kotlin compiler crash, update code to work with new `by` delegation semantics

--- jdom/src/main/kotlin/org/jonnyzzz/kotlin/xml/bind/jdom/impl/bind/XmlBindProperty.kt
+++ jdom/src/main/kotlin/org/jonnyzzz/kotlin/xml/bind/jdom/impl/bind/XmlBindProperty.kt
@@ -9,43 +9,33 @@ import org.jonnyzzz.kotlin.xml.bind.XReadOnly
  * Created by eugene.petrenko@gmail.com
  */
 interface XBindProperty<T : Any> : XProperty<T>, XmlBind {
-  var value : T?
-/*
+  var value: T?
+  override var saveOrder: Int?
+}
+
+//default implementations now has higher priority in `by` syntax,
+//so extracted default implementations to additional interface to avoid side effects
+interface XBindPropertyDefs<T : Any> : XBindProperty<T> {
   override fun minus(defaultValue: T?): XProperty<T> {
     value = defaultValue
     return this
   }
 
   override fun minus(readOnly: XReadOnly): XProperty<T> = object : XBindProperty<T> by this {
-    override fun save(scope : () -> Element) {
+    override fun save(scope: () -> Element) {
       //NOP
     }
   }
 
   override fun minus(event: XCallback<T>): XProperty<T> = object : XBindProperty<T> by this {
-    override fun load(scope : Element?) {
-      this@XBindProperty.load(scope)
+    override fun load(scope: Element?) {
+      this@XBindPropertyDefs.load(scope)
       value = event.onLoaded(value)
     }
-    override fun save(scope : () -> Element) {
+
+    override fun save(scope: () -> Element) {
       value = event.onBeforeSave(value)
-      this@XBindProperty.save(scope)
-    }
-  }*/
-
-  override fun minus(defaultValue: T?): XProperty<T> {
-    value = defaultValue
-    return this
-  }
-
-  override fun minus(readOnly: XReadOnly): XProperty<T> = object : XBindProperty<T> by this {
-    override fun save(scope : () -> Element) {
-      //NOP
+      this@XBindPropertyDefs.save(scope)
     }
   }
-  override fun minus(event: XCallback<T>): XProperty<T> {
-    throw UnsupportedOperationException()
-  }
-
-  override var saveOrder : Int?
 }
--- jdom/src/main/kotlin/org/jonnyzzz/kotlin/xml/bind/jdom/impl/bind/XmlPropertyImpl.kt
+++ jdom/src/main/kotlin/org/jonnyzzz/kotlin/xml/bind/jdom/impl/bind/XmlPropertyImpl.kt
@@ -6,10 +6,10 @@ import kotlin.reflect.KProperty
 /**
  * Created by eugene.petrenko@gmail.com
  */
-abstract class XPropertyImpl<T : Any>(public override var value : T?,
-                                      public override var saveOrder : Int? = null) : XBindProperty<T> {
-  operator public final override fun getValue(thisRef : Any, property : KProperty<*>) : T? = value
-  operator public final override fun setValue(thisRef : Any, property : KProperty<*>, value : T?) {
+abstract class XPropertyImpl<T : Any>(public override var value: T?,
+                                      public override var saveOrder: Int? = null) : XBindPropertyDefs<T> {
+  operator public final override fun getValue(thisRef: Any, property: KProperty<*>): T? = value
+  operator public final override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) {
     this@XPropertyImpl.value = value
   }
 
@@ -22,6 +22,6 @@ abstract class XPropertyImpl<T : Any>(public override var value : T?,
     value = loadImpl(scope)
   }
 
-  protected abstract fun saveImpl(value : T, scope : Element)
-  protected abstract fun loadImpl(scope : Element?) : T?
+  protected abstract fun saveImpl(value: T, scope: Element)
+  protected abstract fun loadImpl(scope: Element?): T?
 }
