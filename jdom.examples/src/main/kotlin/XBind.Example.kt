6f2ed06ee628b962858c24aeed7f3176dd664b1d
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-01-02 23:32:00 +0100

    fix example

--- jdom.examples/src/main/kotlin/XBind.Example.kt
+++ jdom.examples/src/main/kotlin/XBind.Example.kt
@@ -1,25 +1,32 @@
+
 import org.jonnyzzz.kotlin.xml.bind.*
 import org.jonnyzzz.kotlin.xml.bind.jdom.JDOM
+import org.jonnyzzz.kotlin.xml.bind.jdom.JXML
 import org.jonnyzzz.kotlin.xml.bind.jdom.XUnknown
 
 
 class Example {
-  var content by JDOM / "aaa" / "ggg" / XText
-  var attribute by JDOM.saveOrder(5) / "aaa" / XAttribute("yohoho")
-  var sub by JDOM / "sub" / XSub(Sub::class.java)
+  var content by JXML / "aaa" / "ggg" / XText
+  var attribute by JXML.saveOrder(5) / "aaa" / XAttribute("yohoho")
+  var sub by JXML / "sub" / XSub(Sub::class.java)
 
-  var collection by JDOM / "parameters" / XElements("param") / "aaa" / XText
-  var any by JDOM / "parameters" / XAnyElements / XSub(Sub::class.java)
+  var collection by JXML / "parameters" / XElements("param") / "aaa" / XText
+  var any by JXML / "parameters" / XAnyElements / XSub(Sub::class.java)
 }
 
 class Sub {
-  var key by JDOM[2] / XAttribute("key")
-  var value by JDOM[1] / XAttribute("value") - "defaultValue"
+  var key by JXML[2] / XAttribute("key")
+  var value by JXML[1] / XAttribute("value") - "defaultValue"
 
-  var unknown by JDOM / XUnknown
+  var unknown by JXML / XUnknown
 }
 
 fun test() {
   val e = Example()
-  e.sub = Sub()
+
+  /// save to XML
+  val saved = JDOM.save(e)
+
+  /// load from XML
+  val copy = JDOM.load(saved, Example::class.java)
 }
